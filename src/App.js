dc74286875a2738733cb050cd81b27d17aadcc2e
jackalotUVM <gonzalezc9999@gmail.com>
2022-05-02 08:15:38 -0400

    made app.js look similar to 0:47 of the video

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-     
+import ReactDom from 'react-dom'
+import Counter from './counter'
+import React from 'react';
+class  App extends React.Component {
+  render() {
+  return <div className="App">
+      <Counter/>
     </div>
-  );
+  _};
 }
 
 export default App;
