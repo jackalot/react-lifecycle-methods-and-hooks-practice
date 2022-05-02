dd06f9858504122a2969ba87d02891a433f1fd5a
jackalotUVM <gonzalezc9999@gmail.com>
2022-05-02 08:59:44 -0400

    added componentDIdUpdate, and componentDidMount, added changes up to 4:38 of the video

--- src/components/counter.js
+++ src/components/counter.js
@@ -12,6 +12,11 @@ export default class Counter extends React.Component {
         this.increment = () => this.setState({counter: this.state.counter+1})
         this.decrement = () => this.setState({counter: this.state.counter-1})
     }
+
+    componentDidMount() {
+        console.log('Component Did Mount');
+        console.log('-------------------');
+    }
     render () {
         console.log('Render')
 
@@ -22,5 +27,10 @@ export default class Counter extends React.Component {
                 Counter: {this.state.counter}
             </div>
         </div>
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log('Component Did update');
+        console.log('--------------------');
     }
 }
